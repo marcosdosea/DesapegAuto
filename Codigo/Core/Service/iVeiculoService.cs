9344320ff36af01a180c4316213c75a2a6d798e0
luigibarros <luigibarros315@gmail.com>
2025-08-20 22:18:33 -0300

    Foi implementado como solicitado o VeiculoDTO.cs no Core. Closed. #87

---
+++ Codigo/Core/Service/iVeiculoService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTO;
+using System.Collections.Generic;
+
+namespace Core.Service
+{
+    public interface IVeiculoService
+    {
+        int Create(Veiculo veiculo);
+        void Edit(Veiculo veiculo);
+        void Delete(int id);
+        Veiculo? Get(int id);
+        IEnumerable<Veiculo> GetAll();
+        IEnumerable<VeiculoDTO> GetByConcessionaria(int idConcessionaria);
+        IEnumerable<VeiculoDTO> GetByAno(int ano);
+        IEnumerable<VeiculoDTO> GetByQuilometragem(int quilometragem);
+        IEnumerable<VeiculoDTO> GetByPreco(decimal preco);
+        IEnumerable<VeiculoDTO> GetByPlaca(string placa);
+    }
+}
