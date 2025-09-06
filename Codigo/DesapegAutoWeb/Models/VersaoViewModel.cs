25fe79917eb4b1ae2d382c7dec8894ba3c4e9ced
Diego <diego.inocenciio.79@gmail.com>
2025-09-05 21:17:34 -0300

    Criado o Versão View Model. Closed. #135

---
+++ Codigo/DesapegAutoWeb/Models/VersaoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DesapegAutoWeb.Models
+{
+    public class VersaoViewModel
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome da versão é obrigatório.")]
+        [Display(Name = "Nome da Versão")]
+        [StringLength(50, ErrorMessage = "O nome da versão deve ter no máximo 50 caracteres.")]
+        public string Nome { get; set; } = null!;
+
+        [Required(ErrorMessage = "O modelo do veículo é obrigatório.")]
+        [Display(Name = "Modelo do Veículo")]
+        public int IdModelo { get; set; }
+    }
+}
