b4c33f8e3505b20bf54e5e359857f70f6748855f
Diego <diego.inocenciio.79@gmail.com>
2025-09-06 13:32:52 -0300

    Implementado Controller para Versão e o Mappers para Versão. Closed. #137

---
+++ Codigo/DesapegAutoWeb/Mappers/VersaoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using DesapegAutoWeb.Models;
+
+namespace DesapegAutoWeb.Mappers
+{
+    public class VersaoProfile : Profile
+    {
+        public VersaoProfile()
+        {
+            CreateMap<VersaoViewModel, Versao>().ReverseMap();
+        }
+    }
+}
