71584deccd76ca8f22b4ea94653fb640cada7629
CassianoMn <cassianomenezes1@gmail.com>
2025-09-09 09:57:44 -0300

    Implementando Controller, view model e mapper para Categoria. resolves #132

---
+++ Codigo/DesapegAutoWeb/Mappers/CategoriaProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using DesapegAutoWeb.Models;
+
+namespace DesapegAutoWeb.Mappers
+{
+    public class CategoriaProfile : Profile
+    {
+        public CategoriaProfile()
+        {
+            CreateMap<CategoriaViewModel, Categoria>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/DesapegAutoWeb/Models/CategoriaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DesapegAutoWeb.Models
+{
+    public class CategoriaViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome da categoria é obrigatório.")]
+        [Display(Name = "Nome da Categoria")]
+        public string Nome { get; set; } = null!;
+
+    }
+}
